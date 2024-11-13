481db5ccbdf5ba3a4fe3868c385071c886630fac
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2024-11-13 22:57:10 +0530

    java script overview

---
+++ Lec-15 13nov/app.js
@@ -0,0 +1,60 @@
+// let abc=10;
+// console.log(abc);
+
+//_____________________________
+// numbers
+// let a=10;
+// let b=10.15;
+// let c=+Infinity;
+// let d=-Infinity;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(a, b, c, d);
+
+//_____________________________
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+//___________________________________________________________________________
+//String -> 3 
+
+// let a='krishna'
+// let b="bhardwaj"
+// let c=`sharma ji`//(backticks)
+
+// console.log(a, b, c);
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+
+//___________________________________________________________________________
+
+// let naam="samarth";
+// let kaam="time pass";
+// let state="new delhi";
+
+//concatination :- add the strings
+
+// let out = 'hi i am '+naam+' my job is to do '+kaam+' and i put up in '+state
+// console.log(out);
+
+//concatenation nhi krte : it is very bad way
+
+//string template literals
+// let out2= `hi i am ${naam} my job is to do ${kaam} and i put up in ${state}`
+// console.log(out2);
+
+//___________________________________________________________________________
+
+// + - * / % **
+
+let a=5;
+let b=2;
+console.log(a/b);
+console.log(a%b);
